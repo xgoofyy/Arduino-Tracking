debc302cde3cd087b81d22c511c88398cc82f4d5
BuildTools <unconfigured@null.spigotmc.org>
2024-01-01 02:42:53 -0500

    Added cv2 webcam into the canvas

--- mouseTracking.py
+++ mouseTracking.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import *
+import cv2
+from PIL import ImageTk, Image
 
 class App():
     def __init__(self):
@@ -11,20 +13,19 @@ class App():
         self.frame = tk.Frame(self.root)
         self.frame.pack(fill="both", expand=True)
 
-        self.canvasWidth = 720
+        self.canvasWidth = 750
         self.canvasHeight = 540
 
         self.canvas = tk.Canvas(self.frame, width=self.canvasWidth, height=self.canvasHeight, bg="#333333", cursor="none")
         self.canvas.pack()
 
-        self.canvas.create_line(0, self.canvasHeight/2, self.canvasWidth, self.canvasHeight/2, fill="white", width=2) # x axis
-        self.canvas.create_line(self.canvasWidth/2, 0, self.canvasWidth/2,self.canvasHeight, fill="white", width=2) # y axis
-
-        self.crosshairImg = PhotoImage(file="img/crosshair.png")
-        self.crosshair = self.canvas.create_image(self.canvasWidth/2, self.canvasHeight/2, image=self.crosshairImg)
+        self.cap = cv2.VideoCapture(0)
+        self.photo = None
+        self.root.after(1, self.videoUpdate)
 
         self.cords = tk.Label(self.frame, text="", font=("Gotham-Bold", 20))
         self.cords.pack(pady=10)
+        self.mousePos = (0,0)
 
         self.canvas.bind("<Motion>", self.move)
 
@@ -33,12 +34,30 @@ class App():
     def move(self, e):
         x = int(e.x - self.canvasWidth/2)
         y = int(-(e.y - self.canvasHeight/2))
-    
-        self.crosshairImg = PhotoImage(file="img/crosshair.png")
-        self.crosshair = self.canvas.create_image(e.x, e.y, image=self.crosshairImg)
         self.cords.config(text="x: " + str(x) + " y: " + str(y))
+    
+        self.mousePos = (e.x, e.y)
 
+    def videoUpdate(self):
+        ret, frame = self.cap.read()
+        self.crop = 50
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = cv2.resize(frame, (self.canvasWidth+(self.crop*2), self.canvasHeight))
+            frame = frame[:, self.crop:-self.crop]
+
+            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.canvas.create_image(0, 0, anchor=NW, image=self.photo)
+
+            # x and y axis
+            self.canvas.create_line(0, self.canvasHeight/2, self.canvasWidth, self.canvasHeight/2, fill="white", width=2) # x axis
+            self.canvas.create_line(self.canvasWidth/2, 0, self.canvasWidth/2,self.canvasHeight, fill="white", width=2) # y axis
+
+            # draw crosshair
+            self.crosshairImg = PhotoImage(file="img/crosshair.png")
+            self.crosshair = self.canvas.create_image(self.mousePos[0], self.mousePos[1], image=self.crosshairImg)
+
+        self.root.after(1, self.videoUpdate)
 
 if __name__ == "__main__":
     App()
-
